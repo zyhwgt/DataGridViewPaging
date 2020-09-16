7264a603c7d9eb35327155aeb3c9955c59ea4df6
张 勇往直前 <1277893183@qq.com>
2020-09-16 21:37:42 +0800

    添加项目文件。

---
+++ DatagridviewPageSorterDemo/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DatagridviewPageSorterDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labPageIndex = new System.Windows.Forms.Label();
+            this.labRecordCount = new System.Windows.Forms.Label();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.asset_num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.model_num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.asset_num,
+            this.name,
+            this.model_num});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 8);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(655, 577);
+            this.dataGridView1.TabIndex = 24;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // labPageIndex
+            // 
+            this.labPageIndex.AutoSize = true;
+            this.labPageIndex.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labPageIndex.ForeColor = System.Drawing.Color.White;
+            this.labPageIndex.Location = new System.Drawing.Point(12, 630);
+            this.labPageIndex.Name = "labPageIndex";
+            this.labPageIndex.Size = new System.Drawing.Size(103, 16);
+            this.labPageIndex.TabIndex = 53;
+            this.labPageIndex.Text = "当前页：1/1";
+            // 
+            // labRecordCount
+            // 
+            this.labRecordCount.AutoSize = true;
+            this.labRecordCount.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labRecordCount.ForeColor = System.Drawing.Color.White;
+            this.labRecordCount.Location = new System.Drawing.Point(12, 600);
+            this.labRecordCount.Name = "labRecordCount";
+            this.labRecordCount.Size = new System.Drawing.Size(85, 16);
+            this.labRecordCount.TabIndex = 54;
+            this.labRecordCount.Text = "总行数：0";
+            // 
+            // btnLast
+            // 
+            this.btnLast.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(141)))), ((int)(((byte)(199)))));
+            this.btnLast.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLast.ForeColor = System.Drawing.Color.White;
+            this.btnLast.Location = new System.Drawing.Point(546, 610);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(90, 35);
+            this.btnLast.TabIndex = 49;
+            this.btnLast.Text = "末页";
+            this.btnLast.UseVisualStyleBackColor = false;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(141)))), ((int)(((byte)(199)))));
+            this.btnNext.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNext.ForeColor = System.Drawing.Color.White;
+            this.btnNext.Location = new System.Drawing.Point(440, 610);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(90, 35);
+            this.btnNext.TabIndex = 50;
+            this.btnNext.Text = "下一页";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(141)))), ((int)(((byte)(199)))));
+            this.btnPrev.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPrev.ForeColor = System.Drawing.Color.White;
+            this.btnPrev.Location = new System.Drawing.Point(327, 610);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(90, 35);
+            this.btnPrev.TabIndex = 51;
+            this.btnPrev.Text = "上一页";
+            this.btnPrev.UseVisualStyleBackColor = false;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(141)))), ((int)(((byte)(199)))));
+            this.btnFirst.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFirst.ForeColor = System.Drawing.Color.White;
+            this.btnFirst.Location = new System.Drawing.Point(222, 610);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(90, 35);
+            this.btnFirst.TabIndex = 52;
+            this.btnFirst.Text = "首页";
+            this.btnFirst.UseVisualStyleBackColor = false;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // asset_num
+            // 
+            this.asset_num.HeaderText = "资产编号";
+            this.asset_num.Name = "asset_num";
+            this.asset_num.ReadOnly = true;
+            this.asset_num.Width = 200;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "资产名称";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 200;
+            // 
+            // model_num
+            // 
+            this.model_num.HeaderText = "规格型号";
+            this.model_num.Name = "model_num";
+            this.model_num.ReadOnly = true;
+            this.model_num.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(141)))), ((int)(((byte)(199)))));
+            this.ClientSize = new System.Drawing.Size(666, 657);
+            this.Controls.Add(this.labPageIndex);
+            this.Controls.Add(this.labRecordCount);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labPageIndex;
+        private System.Windows.Forms.Label labRecordCount;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.DataGridViewTextBoxColumn asset_num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn model_num;
+    }
+}
+
---
+++ DatagridviewPageSorterDemo/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DatagridviewPageSorterDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 每页记录数
+        /// </summary>
+        public int pageSize = 100;
+
+        /// <summary>
+        /// 总记录数
+        /// </summary>
+        public int recordCount = 0;
+
+        /// <summary>
+        /// 总页数
+        /// </summary>
+        public int pageCount = 0;
+
+        /// <summary>
+        /// 当前页
+        /// </summary>
+        public int currentPage = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PageSorter();//分页 
+        }
+
+
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+                                                e.RowBounds.Location.Y,
+                                                dgv.RowHeadersWidth - 4,
+                                                e.RowBounds.Height);
+
+
+            TextRenderer.DrawText(e.Graphics, (e.RowIndex + 1).ToString(),
+                                    dgv.RowHeadersDefaultCellStyle.Font,
+                                    rectangle,
+                                    dgv.RowHeadersDefaultCellStyle.ForeColor,
+                                    TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+
+        DataTable table = new DataTable();
+
+        /// <summary>
+        /// 分页的方法
+        /// </summary>
+        /// <param name="str"></param>
+        private void PageSorter()
+        {
+
+            //创建虚拟表
+            DataColumn column1 = new DataColumn("test1", Type.GetType("System.String"));
+            DataColumn column2 = new DataColumn("test2", Type.GetType("System.String"));
+            DataColumn column3 = new DataColumn("test3", Type.GetType("System.String"));
+
+            table.Columns.Add(column1);             //将列添加到table表中
+            table.Columns.Add(column2);
+            table.Columns.Add(column3);
+            for (int i = 1; i <= 30000; i++)
+            {
+                DataRow dr = table.NewRow();            //table表创建行
+                dr["test1"] = "资产编号" + i.ToString();
+                dr["test2"] = "资产名称" + i.ToString();
+                dr["test3"] = "规格型号" + i.ToString();
+                table.Rows.Add(dr);                     //将数据加入到table表中
+            }
+
+            recordCount = table.Rows.Count;     //记录总行数
+            pageCount = (recordCount / pageSize);
+            if ((recordCount % pageSize) > 0)
+            {
+                pageCount++;
+            }
+
+            //默认第一页
+            currentPage = 1;
+
+            LoadPage();//调用加载数据的方法
+        }
+
+        /// <summary>
+        /// LoadPage方法
+        /// </summary>
+        private void LoadPage()
+        {
+            if (currentPage < 1) currentPage = 1;
+            if (currentPage > pageCount) currentPage = pageCount;
+
+            int beginRecord;    //开始指针
+            int endRecord;      //结束指针
+            DataTable dtTemp;
+            dtTemp = table.Clone();
+
+            beginRecord = pageSize * (currentPage - 1);
+            if (currentPage == 1) beginRecord = 0;
+            endRecord = pageSize * currentPage;
+
+            if (currentPage == pageCount) endRecord = recordCount;
+            for (int i = beginRecord; i < endRecord; i++)
+            {
+                dtTemp.ImportRow(table.Rows[i]);
+            }
+
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < dtTemp.Rows.Count; i++)
+            {
+                dataGridView1.Rows.Add(new object[] { dtTemp.Rows[i][0], dtTemp.Rows[i][1], dtTemp.Rows[i][2] });
+            }
+
+            labPageIndex.Text = "当前页: " + currentPage.ToString() + " / " + pageCount.ToString();//当前页
+            labRecordCount.Text = "总行数: " + recordCount.ToString() + " 行";//总记录数
+        }
+
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            if (currentPage == 1)
+            { return; }
+            currentPage = 1;
+            LoadPage();
+        }
+        /// <summary>
+        /// 上一页
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (currentPage == 1)
+            { return; }
+            currentPage--;
+            LoadPage();
+        }
+        /// <summary>
+        /// 下一页
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (currentPage == pageCount)
+            { return; }
+            currentPage++;
+            LoadPage();
+        }
+        /// <summary>
+        /// 尾页
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            if (currentPage == pageCount)
+            { return; }
+            currentPage = pageCount;
+            LoadPage();
+        }
+    }
+}
